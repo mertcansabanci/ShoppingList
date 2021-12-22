39c0563bfeb138d6411153c0790a159ab527212e
mertcansabanci <mertcan.sabanci@pru.edu.tr>
2021-12-22 15:55:35 +0300

    Mobile Programming Project

---
+++ app/src/main/java/com/example/shoppinglist/ShowItemsListActivity.java
@@ -0,0 +1,123 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.shoppinglist.db.Items;
+import com.example.shoppinglist.viewmodel.ShowItemListActivityViewModel;
+
+import java.util.List;
+
+public class ShowItemsListActivity extends AppCompatActivity implements ItemsListAdapter.HandleItemsClick {
+
+    private int category_id;
+    private ItemsListAdapter itemsListAdapter;
+    private ShowItemListActivityViewModel viewModel;
+    private RecyclerView recyclerView;
+    private Items itemToUpdate = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_items_list);
+
+        category_id = getIntent().getIntExtra("category_id", 0);
+        String categoryName = getIntent().getStringExtra("category_name");
+
+        getSupportActionBar().setTitle(categoryName);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final EditText addNewItemInput = findViewById(R.id.addNewItemInput);
+        ImageView saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String itemName = addNewItemInput.getText().toString();
+                if(TextUtils.isEmpty(itemName)) {
+                    Toast.makeText(ShowItemsListActivity.this, "Enter Item Name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(itemToUpdate == null)
+                    saveNewItem(itemName);
+                else
+                    updateNewItem(itemName);
+            }
+        });
+        initRecyclerView();
+        initViewModel();
+
+
+    }
+
+    private void initViewModel() {
+        viewModel = new ViewModelProvider(this).get(ShowItemListActivityViewModel.class);
+        viewModel.getItemsListObserver().observe(this, new Observer<List<Items>>() {
+            @Override
+            public void onChanged(List<Items> items) {
+                if(items == null) {
+                    recyclerView.setVisibility(View.GONE);
+                    findViewById(R.id.noResult).setVisibility(View.VISIBLE);
+                } else {
+                    itemsListAdapter.setCategoryList(items);
+                    findViewById(R.id.noResult).setVisibility(View.GONE);
+                    recyclerView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        itemsListAdapter = new ItemsListAdapter(this, this);
+        recyclerView.setAdapter(itemsListAdapter);
+    }
+
+    private void saveNewItem(String itemName) {
+        Items item = new Items();
+        item.itemName = itemName;
+        item.categoryId = category_id;
+        viewModel.insertItems(item);
+        ((EditText) findViewById(R.id.addNewItemInput)).setText("");
+    }
+
+    @Override
+    public void itemClick(Items item) {
+        if(item.complated) {
+            item.complated = false;
+        }
+        else {
+            item.complated = true;
+        }
+        viewModel.updateItems(item);
+    }
+
+    @Override
+    public void removeItem(Items item) {
+        viewModel.deleteItems(item);
+    }
+
+    @Override
+    public void editItem(Items item) {
+        this.itemToUpdate = item;
+        ((EditText) findViewById(R.id.addNewItemInput)).setText(item.itemName);
+    }
+
+    private void updateNewItem(String newName) {
+        itemToUpdate.itemName = newName;
+        viewModel.updateItems(itemToUpdate);
+        ((EditText) findViewById(R.id.addNewItemInput)).setText("");
+        itemToUpdate = null;
+    }
+}
